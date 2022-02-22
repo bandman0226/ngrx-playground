398a171493c48d1ccd9b193e661ddc0a1af942e9
Junior LeVey <jlevey@paperwise.com>
2022-02-22 17:58:14 -0600

    init commit

---
+++ libs/books-page/src/lib/book-api.effects.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { mergeMap, map, exhaustMap, concatMap } from 'rxjs/operators';
+import { BooksService } from '@book-co/shared-services';
+import { BooksPageActions, BooksApiActions } from '@book-co/books-page/actions';
+
+@Injectable()
+export class BooksApiEffects {
+    constructor(private booksService: BooksService, private actions$: Actions) {
+    }
+
+    loadBooks$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(BooksPageActions.enter),
+            exhaustMap(() => {
+                return this.booksService
+                    .all()
+                    .pipe(map((books) => BooksApiActions.booksLoaded({books})))
+            })
+        )
+    });
+
+    createBook$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(BooksPageActions.createBook),
+            concatMap((action) =>
+                this.booksService
+                    .create(action.book)
+                    .pipe(map((book) => BooksApiActions.bookCreated({book})))
+            )
+        )
+    });
+
+    updateBook$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(BooksPageActions.updateBook),
+            concatMap((action) =>
+                this.booksService
+                    .update(action.bookId, action.changes)
+                    .pipe(map((book) => BooksApiActions.bookUpdated({book})))
+            )
+        )
+    );
+
+    deleteBook$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(BooksPageActions.deleteBook),
+            concatMap((action) =>
+                this.booksService
+                    .delete(action.bookId)
+                    .pipe(map(() => BooksApiActions.bookDeleted({bookId: action.bookId})))
+            )
+        )
+    );
+}
